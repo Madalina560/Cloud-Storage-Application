1025be802bd28610c628772f8490d546b3336197
Madalina C <costovim@tcd.ie>
2025-04-12 11:01:48 +0100

    Added decryption functionallity

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template, request, redirect, send_file
 from flask_sqlalchemy import SQLAlchemy
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+import sys
 
 # file uploading & downloading tutorial followed:
 # https://www.geeksforgeeks.org/uploading-and-downloading-files-in-flask/
+
+# encryption and decryption example used:
+# https://pycryptodome.readthedocs.io/en/latest/src/examples.html#encrypt-and-authenticate-data-in-one-step:~:text=.decode())-,Encrypt%20and%20authenticate%20data%20in%20one%20step,-%C2%B6
 
 aesKey = get_random_bytes(16)
 
@@ -29,11 +33,11 @@ def index():
         ciphertext, tag = cipher.encrypt_and_digest(upload.data)
         assert len(cipher.nonce) == 15
 
-        # encryption
+        # putting together encrypted data + uploading it
         encryptedData = tag + cipher.nonce + ciphertext
         encryptUpload = Upload(filename = file.filename + ".enc", data = encryptedData)
 
-        db.session.add(encryptUpload)  # upload encrypted file
+        db.session.add(encryptUpload)  # add encrypted file to db
         db.session.commit()
         return redirect('/')
     files = Upload.query.all()
@@ -42,7 +46,23 @@ def index():
 @app.route('/download/<upload_id>')
 def download_file(upload_id):
     upload = Upload.query.filter_by(id = upload_id).first()
-    return send_file(BytesIO(upload.data), download_name=upload.filename, as_attachment=True)
+
+    binary = upload.data
+    tag = binary[:16]
+    nonce = binary[16:31]
+    ciphertext = binary[31:]
+    
+    cipher = AES.new(aesKey, AES.MODE_OCB, nonce = nonce)
+
+    try:
+        decryptedData = cipher.decrypt_and_verify(ciphertext, tag)
+        originalFile = upload.filename.replace('.enc', '')
+        return send_file(BytesIO(decryptedData), download_name=originalFile, as_attachment=True)
+
+    
+    except ValueError:
+        print("File Modified")
+        return "Decryption failed, File may have been tampered with.", 400
 
 if __name__ == '__main__':
     db.create_all()
